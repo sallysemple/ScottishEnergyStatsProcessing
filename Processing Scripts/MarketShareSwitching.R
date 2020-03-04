bfcd9f2c40eeeaa39dd88b930efe3f0bd87aca0f
ischerr <iainscherr@gmail.com>
2020-03-04 14:32:46 +0000

    Marketshare Correction

--- Processing Scripts/MarketShareSwitching.R
+++ Processing Scripts/MarketShareSwitching.R
@@ -1,15 +1,17 @@
 library(readxl)
 library(readr)
+library(tibble)
 
 print("MarketShareSwitching")
 
 
 MarketShare <- read_excel("Data Sources/Consumer Stats/MarketShareSwitching.xlsx", 
-                                   skip = 2, n_max = 3)
+                                   skip = 2, n_max = 4, col_names = FALSE)
+MarketShare <- as_tibble(t(MarketShare))
 
-names(MarketShare)[1] <- "Size"
+names(MarketShare) <- c("Region", "Large", "Medium", "Small")
 
-MarketShare[1,1] <- "Large"
+MarketShare <- MarketShare[-1,]
 
 write_csv(MarketShare, "Output/Consumers/MarketShare.csv")
 
