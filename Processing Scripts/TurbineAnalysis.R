5fd2f5adbe73809b549b3a581b1ce69741e49bd6
ischerr <iainscherr@gmail.com>
2020-04-17 11:48:41 +0100

    New Processes

---
+++ Processing Scripts/TurbineAnalysis.R
@@ -0,0 +1,152 @@
+library(readxl)
+library(plyr)
+library(readr)
+library("writexl")
+library(lubridate)
+library(reshape2)
+
+### Open Excel File and Extract Relevant Data ###
+
+## read_excel reads .xlsx files, read_csv reads .csv files. ##
+## If using csv file directly from the website, include the skip argument to remove excess lines at the top. ##
+
+CurrentData <- read_excel("Data Sources/REPD (Turbines)/Source/Current.xlsx",
+                          sheet = "Database",
+                          skip = 5)
+
+### Add in site count of 1 for each site, for aggregation later ###
+
+CurrentData$Sites <- 1
+
+CurrentCorrections <- read_excel("Data Sources/REPD (Turbines)/Corrections/CurrentCorrections.xlsx",
+                                 sheet = "Database",
+                                 skip = 5)
+
+#CurrentCorrections$Sites <- 0
+
+CurrentData <- rbind(CurrentData,CurrentCorrections)
+
+### Read Source Data ###
+#CurrentData <- read_csv("Data Sources/REPD/Current.csv", skip = 6)
+
+### Create Scottish Subset ###
+ScotlandCurrent <- subset(CurrentData, Country == "Scotland")
+
+### Rename Variables ###
+ScotlandCurrent <-
+  plyr::rename(
+    ScotlandCurrent,
+    c(
+      "Technology Type" = "TechType",
+      "Development Status (short)" =
+        "Status",
+      "No. of Turbines" = "TurbineAmount",
+      "Installed Capacity (MWelec)" =
+        "Capacity"
+    )
+  )
+
+### Create Subset of Turbines of the correct status.###
+## Vertical Line is an OR operator ##
+
+### Subset only Wind Tech ###
+ScotTurbineCurrent <-
+  subset(ScotlandCurrent,
+         TechType == "Wind Onshore" | TechType == "Wind Offshore")
+
+### Keep only required Status ###
+ScotTurbineCurrent <- subset(
+  ScotTurbineCurrent,
+  Status == "Operational"
+  | Status == "Awaiting Construction"
+  | Status == "Under Construction"
+  | Status == "Application Submitted"
+  ,  select = c(TechType, Status, Sites, TurbineAmount, Capacity)
+)
+
+### Convert to Number fields and set missing values to 0, for calculations ###
+ScotTurbineCurrent$TurbineAmount <-
+  as.numeric(ScotTurbineCurrent$TurbineAmount)
+
+### Change NA values to 0 ###
+ScotTurbineCurrent$TurbineAmount[is.na(ScotTurbineCurrent$TurbineAmount)] <-
+  0
+
+### Make Capacity Column numeric ###
+ScotTurbineCurrent$Capacity <-
+  as.numeric(ScotTurbineCurrent$Capacity)
+
+### Make NA Values 0 ###
+ScotTurbineCurrent$Capacity[is.na(ScotTurbineCurrent$Capacity)] <- 0
+
+### Split data into Onshore and Offshore ###
+
+### Offshore Subset ###
+OffshoreCurrent <-
+  subset(ScotTurbineCurrent, TechType == "Wind Offshore")
+
+### Onshore Subset ###
+OnshoreCurrent <-
+  subset(ScotTurbineCurrent, TechType == "Wind Onshore")
+
+### Sum up Numerical Columns, to give totals ###
+
+CurrentOffshoreTable  <-
+  ddply(OffshoreCurrent, .(Status), numcolwise(sum))
+
+CurrentOnshoreTable   <-
+  ddply(OnshoreCurrent, .(Status), numcolwise(sum))
+
+### Export to CSV Files ###
+
+
+write.table(
+  CurrentOffshoreTable,
+  "Output/Turbine Analysis/Current/CurrentOffshore.txt",
+  sep = "\t",
+  row.names = FALSE
+)
+
+write.table(
+  CurrentOnshoreTable,
+  "Output/Turbine Analysis/Current/CurrentOnshore.txt",
+  sep = "\t",
+  row.names = FALSE
+)
+
+CurrentAllWindTable <-
+  ddply(ScotTurbineCurrent, .(Status), numcolwise(sum))
+
+Date <- as.character(CurrentData$`Record Last Updated (dd/mm/yyyy)`)
+
+Date <- ymd(max(Date[which(substr(Date,6,7) %in% c("03", "06", "09", "12"))])) 
+
+
+CurrentAllWindTable$Month <- format(Date, "%b-%y")
+
+
+### Sites
+
+TimeSeriesSites <- CurrentAllWindTable[c(5,1,2)]
+
+TimeSeriesSites <- dcast(TimeSeriesSites, Month ~ Status, value.var = "Sites")
+
+write.csv(TimeSeriesSites, paste0("Output/Turbine Analysis/Time Series/Sites/",format(Date, "%b-%y"),".csv"))
+
+### Turbines
+
+TimeSeriesTurbines <- CurrentAllWindTable[c(5,1,3)]
+
+TimeSeriesTurbines <- dcast(TimeSeriesTurbines, Month ~ Status, value.var = "TurbineAmount")
+
+write.csv(TimeSeriesTurbines, paste0("Output/Turbine Analysis/Time Series/Turbines/",format(Date, "%b-%y"),".csv"))
+
+
+###Capacity
+
+TimeSeriesCapacity <- CurrentAllWindTable[c(5,1,4)]
+
+TimeSeriesCapacity <- dcast(TimeSeriesCapacity, Month ~ Status, value.var = "Capacity")
+
+write.csv(TimeSeriesCapacity, paste0("Output/Turbine Analysis/Time Series/Capacity/",format(Date, "%b-%y"),".csv"))
+
---
+++ Processing Scripts/UnitBillCosts.R
@@ -0,0 +1,21 @@
+library(readxl)
+
+ElectricityUnit <- read_excel("Data Sources/Energy Bills/ElectricityUnit.xlsx", 
+                              sheet = "Standard", skip = 4)[c(1,2,3,5,6,8,9,11,12)]
+
+names(ElectricityUnit) <- c("Region", "Credit - Average variable unit price (£/kWh)", "Credit - Average fixed cost (£/year)", "Direct debit - Average variable unit price (£/kWh)", "Direct debit - Average fixed cost (£/year)", "Prepayment - Average variable unit price (£/kWh)", "Prepayment - Average fixed cost (£/year)", "Total - Average variable unit price (£/kWh)", "Total - Average fixed cost (£/year)")
+
+ElectricityUnit <- ElectricityUnit[which(ElectricityUnit$Region %in% c("North Scotland", "South Scotland")),]
+
+write.csv(ElectricityUnit, "Output/Energy Bills/ElecUnitCost.csv", row.names = FALSE)
+
+
+
+GasUnit <- read_excel("Data Sources/Energy Bills/GasUnit.xlsx", 
+                              sheet = "Standard", skip = 4)[c(1,2,3,5,6,8,9,11,12)]
+
+names(GasUnit) <- c("Region", "Credit - Average variable unit price (£/kWh)", "Credit - Average fixed cost (£/year)", "Direct debit - Average variable unit price (£/kWh)", "Direct debit - Average fixed cost (£/year)", "Prepayment - Average variable unit price (£/kWh)", "Prepayment - Average fixed cost (£/year)", "Total - Average variable unit price (£/kWh)", "Total - Average fixed cost (£/year)")
+
+GasUnit <- GasUnit[which(GasUnit$Region %in% c("North Scotland", "South Scotland")),]
+
+write.csv(GasUnit, "Output/Energy Bills/GasUnitCost.csv", row.names = FALSE)
