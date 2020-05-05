39bb1d353884e61928f26ee1ae28d85f5e118bdf
ischerr <iainscherr@gmail.com>
2020-05-05 16:24:50 +0100

    Value of services and assets

---
+++ Processing Scripts/ValueServicesAssets.R
@@ -0,0 +1,46 @@
+library(readxl)
+library(tidyverse)
+
+
+FossilFuels <- read_excel("Data Sources/Natural Capital/referencetables.xlsx", 
+                                   sheet = "Fossil fuels", col_names = FALSE, 
+                                   skip = 2)
+
+FossilFuels <-  as_tibble(t(FossilFuels))
+
+FossilFuels <- FossilFuels[c(1,4,6)]
+
+names(FossilFuels) <- c("Year", "Annual", "Asset")
+
+FossilFuels$Year <- as.numeric(FossilFuels$Year)
+FossilFuels$Annual <- as.numeric(FossilFuels$Annual)/1000
+FossilFuels$Asset <- as.numeric(FossilFuels$Asset)/1000
+
+FossilFuels <- FossilFuels[complete.cases(FossilFuels),]
+
+write.table(FossilFuels,
+            "Output/Services and assets/FossilFuels.txt",
+            sep = "\t",
+            row.names = FALSE)
+
+
+Renewables <- read_excel("Data Sources/Natural Capital/referencetables.xlsx", 
+                              sheet = "Renewables", col_names = FALSE, 
+                              skip = 2)
+
+Renewables <-  as_tibble(t(Renewables))
+
+Renewables <- Renewables[c(1,4,6)]
+
+names(Renewables) <- c("Year", "Annual", "Asset")
+
+Renewables$Year <- as.numeric(Renewables$Year)
+Renewables$Annual <- as.numeric(Renewables$Annual)
+Renewables$Asset <- as.numeric(Renewables$Asset)/1000
+
+Renewables <- Renewables[complete.cases(Renewables),]
+
+write.table(Renewables,
+            "Output/Services and assets/FossilFuels.txt",
+            sep = "\t",
+            row.names = FALSE)
