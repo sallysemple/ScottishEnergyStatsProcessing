a09606206ba3be2ef94b5cc394be42270a0725b9
ischerr <iainscherr@gmail.com>
2020-06-01 13:05:04 +0100

    ECO

--- Processing Scripts/ECO.R
+++ Processing Scripts/ECO.R
@@ -1,13 +1,31 @@
 library(readxl)
 library(plyr)
 library(dplyr)
+library(tidyverse)
+library(magrittr)
 
 ECOMeasuresHouseholds <- read_excel("Data Sources/ECO/Current.xlsx", 
                                     sheet = "T4.1", skip = 2)
 
-ECOMeasuresHouseholds <- ECOMeasuresHouseholds[which(ECOMeasuresHouseholds$`Area Codes` == "S92000003"),]
+ECOMeasuresHouseholds[1,] <- names(ECOMeasuresHouseholds)
 
-write.table(ECOMeasuresHouseholds,
+ECOMeasuresHouseholds <- ECOMeasuresHouseholds[which(ECOMeasuresHouseholds$`Area Codes` == "S92000003" | ECOMeasuresHouseholds$`Area Codes` == "Area Codes"),]
+
+ECOMeasuresHouseholds <- as_tibble(t(ECOMeasuresHouseholds))
+
+names(ECOMeasuresHouseholds) <- c("Quarter", "Households")
+
+ECOMeasuresHouseholdsCumulative <- head(ECOMeasuresHouseholds,-4)
+
+ECOMeasuresHouseholdsCumulative$`Households` <- as.numeric(ECOMeasuresHouseholdsCumulative$`Households`)
+
+ECOMeasuresHouseholdsCumulative <- ECOMeasuresHouseholdsCumulative[complete.cases(ECOMeasuresHouseholdsCumulative),]
+
+ECOMeasuresHouseholdsCumulative <- ECOMeasuresHouseholdsCumulative %>% mutate(Households = cumsum(Households))
+
+ECOMeasuresHouseholdsCumulative <- rbind(ECOMeasuresHouseholdsCumulative, ECOMeasuresHouseholds[nrow(ECOMeasuresHouseholds),])
+
+write.table(ECOMeasuresHouseholdsCumulative,
             "Output/ECO/ECOMeasuresHouseholds.txt",
             sep = "\t",
             row.names = FALSE)
@@ -24,3 +42,51 @@ write.table(ECOMeasuresTotals,
             "Output/ECO/ECOMeasuresTotals.txt",
             sep = "\t",
             row.names = FALSE)
+
+ECOMeasuresTotals <- read_excel("Data Sources/ECO/Current.xlsx", 
+                                    sheet = "T3.3", skip = 2)
+
+ECOMeasuresTotals[1,] <- names(ECOMeasuresTotals)
+
+ECOMeasuresTotals <- fill(ECOMeasuresTotals, Obligation, .direction = "down")
+
+ECOMeasuresTotals <- ECOMeasuresTotals[which(ECOMeasuresTotals$`Area Codes` == "S92000003" | ECOMeasuresTotals$`Area Codes` == "Area Codes"),]
+
+ECOMeasuresTotals <- as_tibble(t(ECOMeasuresTotals))
+
+names(ECOMeasuresTotals) <- c("Quarter",
+                                        "CERO",
+                                        "CERO Rural",
+                                        "CSCO",
+                                        "CSCO Rural",
+                                        "Affordable Warmth",
+                                        "Rural",
+                                        "Total"
+)
+ECOMeasuresTotalsCumulative <- head(ECOMeasuresTotals,-4)
+
+
+ECOMeasuresTotalsCumulative[2:8] %<>% lapply(function(x) as.numeric(as.character(x)))
+
+ECOMeasuresTotalsCumulative <- tail(ECOMeasuresTotalsCumulative, -3)
+
+ECOMeasuresTotalsCumulative[is.na(ECOMeasuresTotalsCumulative)] <- 0
+
+ECOMeasuresTotalsCumulative <- ECOMeasuresTotalsCumulative %>% mutate(`CERO` = cumsum(`CERO`),
+                                                                      `CERO Rural` = cumsum(`CERO Rural`),
+                                                                      `CSCO` = cumsum(`CSCO`),
+                                                                      `CSCO Rural` = cumsum(`CSCO Rural`),
+                                                                      `Affordable Warmth` = cumsum(`Affordable Warmth`),
+                                                                      `Rural` = cumsum(`Rural`),
+                                                                      `Total` = cumsum(`Total`))
+
+ECOMeasuresTotalsCumulative <- rbind(ECOMeasuresTotalsCumulative, ECOMeasuresTotals[nrow(ECOMeasuresTotals),])
+
+write.table(ECOMeasuresTotalsCumulative,
+            "Output/ECO/ECOMeasuresTotals.txt",
+            sep = "\t",
+            row.names = FALSE)
+
+
+
+
