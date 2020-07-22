335f5431cdf20c3d763f81f27b19d446f002214c
ischerr <iainscherr@gmail.com>
2020-07-22 14:15:15 +0100

    OperationalCapacityBySize

--- Processing Scripts/OperationalCapBySize.R
+++ Processing Scripts/OperationalCapBySize.R
@@ -5,55 +5,181 @@ library(reshape2)
 library(readr)
 library(lubridate)
 library(zoo)
+library("writexl")
 
 print("OperationalCapBySize")
 
 
-#REPD <- read_excel("C:/Users/ische/Downloads/renewable-energy-planning-database-december-2019.xlsx", 
-#sheet = "Database", skip = 6)
-REPD <- read_delim("Output/REPD (Operational Corrections)/REPD.txt", 
-                      "\t", escape_double = FALSE, trim_ws = TRUE)
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <-
+    lapply(sheets, function(X)
+      readxl::read_excel(filename, sheet = X))
+  if (!tibble)
+    x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
+
+DECCsheets <-
+  read_excel_allsheets("Data Sources/RESTATS/ScotlandCapacity.xlsx")
+
+### Merge Datasets together ###
+DECCsheets <- rbind_list(DECCsheets)
 
 
-REPD <- REPD[c(2,5,6,9,19,23)]
+DECCsheets[is.na(DECCsheets)] <- 0
+### Reorganise Columns ###
+DECCsheets <-
+  select(DECCsheets,
+         "RenewableArea",
+         "ProjectName",
+         "SiteName",
+         "Country",
+         everything())
 
-unique(REPD$`Development Status (short)`)
-unique(REPD$Country)
+### Remove Extra Columns ###
+DECCsheets <-
+  within(
+    DECCsheets,
+    rm(
+      "RESTATS Ref",
+      "ROC Ref",
+      "Operational Month (0=previous years)",
+      "Survey",
+      "CAPACITY END of last year",
+      "CAPACITY END of last"
+    )
+  )
 
-REPD <- subset(REPD, `Technology Type` %in% c("Biomass (co-firing)", "EfW Incineration" ,"Biomass (dedicated)", "Advanced Conversion Technologies", "Anaerobic Digestion", "Large Hydro", "Small Hydro","Landfill Gas", "Solar Photovoltaics", "Sewage Sludge Digestion", "Tidal Barrage and Tidal Stream", "Shoreline Wave", "Wind Offshore", "Wind Onshore", "Hot Dry Rocks (HDR)"))
+####################################################################################################################
+####### THis Code retains the most recent capacity column, and removes rows that have 0 capacity.            #######
+####### This is particularly sensitive to changes in the format of the spreadsheet.                          #######
+####### The code will still run if column names are not exact matches but capacity and 0s will not be tidied #######
+####################################################################################################################
 
-REPD <- REPD %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Tidal Barrage and Tidal Stream", "Shoreline wave / tidal")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Shoreline Wave", "Shoreline wave / tidal")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Large Hydro", "Hydro")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Small Hydro", "Hydro")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Biomass (co-firing)", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "EfW Incineration", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Biomass (dedicated)", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Advanced Conversion Technologies", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Anaerobic Digestion", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Landfill Gas", "Biomass and Waste")) %>% 
-  mutate(`Technology Type` = replace(`Technology Type`, `Technology Type` == "Sewage Sludge Digestion", "Biomass and Waste")) 
+### Code for Q4 commented. Repeat for other Quarters ###
+
+if ("Capacity Q4" %in% colnames(DECCsheets)) # Checks if there is a Capacity Q4 column in the data
+{
+  if (sum(as.numeric(DECCsheets$`Capacity Q4`), na.rm = TRUE) > 0) # Checks if the sum of that column is more than 0
+  {
+    DECCsheets$Capacity <- DECCsheets$`Capacity Q4` # Uses Q4 as the Capacity Column
+    DECCsheets$'Capacity Q4' <- NULL # Removes other Columns
+    DECCsheets$'Capacity Q3' <- NULL
+    DECCsheets$'Capacity Q2' <- NULL
+    DECCsheets$'Capacity Q1' <- NULL
+    
+  } else {
+    DECCsheets$'Capacity Q4' <- NULL # If Column exists but is 0, then removes column and checks Column 3.
+  }
+}
+
+if ("Capacity Q3" %in% colnames(DECCsheets))
+{
+  if (sum(as.numeric(DECCsheets$`Capacity Q3`), na.rm = TRUE) > 0)
+  {
+    DECCsheets$Capacity <- DECCsheets$`Capacity Q3`
+    DECCsheets$'Capacity Q3' <- NULL
+    DECCsheets$'Capacity Q2' <- NULL
+    DECCsheets$'Capacity Q1' <- NULL
+    
+  } else {
+    DECCsheets$'Capacity Q3' <- NULL
+  }
+}
+
+if ("Capacity Q2" %in% colnames(DECCsheets))
+{
+  if (sum(as.numeric(DECCsheets$`Capacity Q2`), na.rm = TRUE) > 0)
+  {
+    DECCsheets$Capacity <- DECCsheets$`Capacity Q2`
+    DECCsheets$'Capacity Q2' <- NULL
+    DECCsheets$'Capacity Q1' <- NULL
+    
+  } else {
+    DECCsheets$'Capacity Q2' <- NULL
+  }
+}
+
+if ("Capacity Q1" %in% colnames(DECCsheets))
+{
+  if (sum(as.numeric(DECCsheets$`Capacity Q1`), na.rm = TRUE) > 0)
+  {
+    DECCsheets$Capacity <- DECCsheets$`Capacity Q1`
+    
+    DECCsheets$'Capacity Q1' <- NULL
+    
+  } else {
+    DECCsheets$'Capacity Q1' <- NULL
+  }
+}
+
+### If a capacity Column is created above, rearrange columns to place it as required ###
+
+if ("Capacity" %in% colnames(DECCsheets)) {
+  DECCsheets <-
+    select(
+      DECCsheets,
+      "RenewableArea",
+      "ProjectName",
+      "SiteName",
+      "Country",
+      "Capacity",
+      everything()
+    )
   
+  
+  DECCsheets[DECCsheets == 0] <- NA # Set all 0 values to NA
+  
+  DECCsheets <- DECCsheets %>% drop_na(Capacity) # Remove rows where Capacity has an NA Value
+}
 
-REPD <- REPD[which(REPD$`Development Status (short)` %in% c("Operational")),]
 
-REPD$CapacityBand <- "50MW +"
+DECCsheets <- DECCsheets %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Anaerobic Digestion", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Photovoltaics", "Solar Photovoltaics")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Small Hydro", "Hydro")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Large Hydro", "Hydro")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Wind Onshore", "Wind Onshore")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Wind Offshore", "Wind Offshore")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Landfill Gas", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Landfill gas", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Sewage Sludge Digestion", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Biomass", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Poultry Litter", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Municipal Solid Waste Combustion", "Biomass and Waste")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Shoreline Wave", "Shoreline wave / tidal")) %>% 
+  mutate(`RenewableArea` = replace(`RenewableArea`, `RenewableArea` == "Tidal", "Shoreline wave / tidal")) 
 
-REPD[which(REPD$`Installed Capacity (MWelec)` < 50),]$CapacityBand <- "10-50MW"
-REPD[which(REPD$`Installed Capacity (MWelec)` < 10),]$CapacityBand <- "5 - 10MW"
-REPD[which(REPD$`Installed Capacity (MWelec)` < 5),]$CapacityBand <- "<5MW"
+DECCsheets <- DECCsheets[1:5]
 
-REPD <- REPD %>%  group_by(`Technology Type`, `Development Status (short)`,  CapacityBand) %>% 
-  summarise(`Installed Capacity (MWelec)` = sum(`Installed Capacity (MWelec)`, na.rm = TRUE))
+DECCsheets$CapacityBand <- "50MW +"
 
-Table <- dcast(REPD, `Technology Type` ~ CapacityBand, value.var = "Installed Capacity (MWelec)")
+DECCsheets[which(DECCsheets$`Capacity` < 50),]$CapacityBand <- "10-50MW"
+DECCsheets[which(DECCsheets$`Capacity` < 10),]$CapacityBand <- "5 - 10MW"
+DECCsheets[which(DECCsheets$`Capacity` < 5),]$CapacityBand <- "<5MW"
+
+DECCsheets[which(substr(DECCsheets$ProjectName,1,4) == "FIT-"),]$CapacityBand <- "<5MW"
+
+DECCsheets <- DECCsheets %>%  group_by(`RenewableArea`,   CapacityBand) %>% 
+  summarise(`Capacity` = sum(`Capacity`, na.rm = TRUE))
+
+Table <- dcast(DECCsheets, `RenewableArea` ~ CapacityBand, value.var = "Capacity")
 
 Table[is.na(Table)] <- 0
 
 Table$Total <- Table$`10-50MW`+Table$`5 - 10MW`+Table$`50MW +`+Table$`<5MW`
 
 Table <- Table[order(-Table$Total),]
+
+names(Table)[1] <- "Technology Type"
+
+###
+
 
 
 
@@ -85,4 +211,5 @@ write.table(Table[c(1,2,4,3,5,6)],
           "Output/Capacity by Size/CapacitySizeTech.txt",
           sep = "\t",
           row.names = FALSE)
+
 
