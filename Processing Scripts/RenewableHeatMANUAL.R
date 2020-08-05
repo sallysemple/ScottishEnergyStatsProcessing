9444ef030001cb2e0f63dc4157a4345ef42448b8
ischerr <iainscherr@gmail.com>
2020-08-05 14:26:55 +0100

    RenHeat

--- Processing Scripts/RenewableHeatMANUAL.R
+++ Processing Scripts/RenewableHeatMANUAL.R
@@ -3,17 +3,17 @@ library(readr)
 
 RenHeatCapOutput <- read_excel("Data Sources/MANUAL/RenHeatCapOutput.xlsx")
 
-RenHeatCapOutput <- RenHeatCapOutput[c(8,1:7),]
+RenHeatCapOutput <- RenHeatCapOutput[c(8,1:7)]
 
 write.table(RenHeatCapOutput,
-            "Output/Renewable Heat/RenHeatCapOutput .txt",
+            "Output/Renewable Heat/RenHeatCapOutput.txt",
             sep = "\t",
             row.names = FALSE)
 
 
 RenHeatEnergyFromWaste <- read_excel("Data Sources/MANUAL/RenHeatEnergyFromWaste.xlsx")
 
-RenHeatEnergyFromWaste <- RenHeatEnergyFromWaste[c(6,1:5),]
+RenHeatEnergyFromWaste <- RenHeatEnergyFromWaste[c(6,1:5)]
 
 write.table(RenHeatEnergyFromWaste,
             "Output/Renewable Heat/RenHeatEnergyFromWaste.txt",
