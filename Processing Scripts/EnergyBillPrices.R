be2a0c2c27d6889442f5b1e144bc0ddacf4b72a2
felixpstats <79103963+felixpstats@users.noreply.github.com>
2022-01-18 15:43:11 +0000

    billchanges

--- Processing Scripts/EnergyBillPrices.R
+++ Processing Scripts/EnergyBillPrices.R
@@ -7,6 +7,74 @@ library(data.table)
 print("EnergyBillPrices")
 
 
+###Electricity
+
+
+#Load Source Data
+library(readxl)
+ElectricityAverageBill <- read_excel("Data Sources/Energy Bills/ElectricityAverageBill.xlsx", 
+                                     sheet = "2.2.2", skip = 7)
+
+
+#Keep Only useful Columns
+ElectricityAverageBill <- select(ElectricityAverageBill,
+                         Year,
+                         "Prepayment: Scotland (pounds)",
+                         "Standard credit: Scotland (pounds)",
+                         "Direct debit: Scotland (pounds)")
+
+#Rename Columns
+names(ElectricityAverageBill) <- c("Year", "Prepayment", "Standard Credit", "Direct Debit")
+
+#Export to CSV
+write_csv(ElectricityAverageBill, "Output/Energy Bills/ElectricityAverageBill.csv")
+
+
+### Electricity Regional ###
+
+ElectricityRegionAverageBill <- read_excel("Data Sources/Energy Bills/ElectricityRegionAverageBill.xlsx", 
+                                   sheet = "2.2.3 (Time Series)", skip = 10)
+
+
+#Keep useful columns
+ElectricityRegionAverageBill <- select(ElectricityRegionAverageBill,
+                               Year,
+                               Region,
+                               "Prepayment: Bill (Pounds)",  
+                               "Credit: Bill (Pounds)",
+                               "Direct debit: Bill (Pounds)",
+                               "Overall: Bill (Pounds)" 
+)
+
+#Rename Columns
+names(ElectricityRegionAverageBill) <- c("Year", "Region", "Prepayment", "Standard Credit", "Direct Debit", "Total")
+
+#Melt into tidy format
+ElectricityRegionAverageBill <- melt(ElectricityRegionAverageBill, id.vars = c("Region", "Year"))
+
+#Export to CSV
+write_csv(ElectricityRegionAverageBill, "Output/Energy Bills/ElectricityRegionAverageBill.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### GAS ###
 
 #Load Source Data
