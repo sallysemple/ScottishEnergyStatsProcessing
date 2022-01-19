efec792c8fdeb5e82fb31ca5bbf9e31cc197ec6a
felixpstats <79103963+felixpstats@users.noreply.github.com>
2022-01-19 10:46:05 +0000

    Fixedtariffchanges

--- Processing Scripts/FixedTariffs.R
+++ Processing Scripts/FixedTariffs.R
@@ -27,3 +27,24 @@ GasPaymentMethod <- dcast(GasPaymentMethod, Quarter ~ Region)
 GasPaymentMethod[2:ncol(GasPaymentMethod)] %<>% lapply(function(x) as.numeric(as.character(x))/100)
 
 write_csv(GasPaymentMethod, "Output/Energy Bills/GasFixedTariff.csv")
+
+###Electricity
+
+#Read Source Data
+ElectricityPaymentMethod <- read_excel("Data Sources/Energy Bills/ElectricityPaymentMethod.xlsx", 
+                               sheet = "2.4.2 (Fixed Quarterly)", skip = 14)
+
+#Keep Useful Columns
+ElectricityPaymentMethod <- select(ElectricityPaymentMethod,
+                           Quarter,
+                           "Region [Note 1]",
+                           "All payment types (%)"
+)
+
+names(ElectricityPaymentMethod) <- c("Quarter", "Region", "Fixed Tariff Proportion")
+
+ElectricityPaymentMethod <- dcast(ElectricityPaymentMethod, Quarter ~ Region)
+
+ElectricityPaymentMethod[2:ncol(ElectricityPaymentMethod)] %<>% lapply(function(x) as.numeric(as.character(x))/100)
+
+write_csv(ElectricityPaymentMethod, "Output/Energy Bills/ElectricityFixedTariff.csv")
