b39b6704063d8e62b75cb4e1a8c8e73612603acf
felixpstats <79103963+felixpstats@users.noreply.github.com>
2022-07-18 11:55:42 +0100

    lcreupdate

--- Processing Scripts/LCRE.R
+++ Processing Scripts/LCRE.R
@@ -2,23 +2,23 @@ library(readxl)
 library(tidyr)
 library(data.table)
 
-lcreedataset2019 <- read_excel("Data Sources/LCRE Survey/lcreedataset2019.xlsx", 
+lcreedataset2020 <- read_excel("Data Sources/LCRE Survey/lcreedataset2020.xlsx", 
                                sheet = "LCRE by country")
 
-lcreedataset2019 <- fill(lcreedataset2019,1, .direction ="down")
+lcreedataset2020 <- fill(lcreedataset2020,1, .direction ="down")
 
-lcreedataset2019 <- as_tibble(lcreedataset2019)
+lcreedataset2020 <- as_tibble(lcreedataset2020)
 
 lcrelist <- list()
 
 
-min(as.numeric(lcreedataset2019[2,]), na.rm = TRUE)
+min(as.numeric(lcreedataset2020[2,]), na.rm = TRUE)
 
-for (year in min(as.numeric(lcreedataset2019[2,]), na.rm = TRUE):max(as.numeric(lcreedataset2019[2,]), na.rm = TRUE)){
+for (year in min(as.numeric(lcreedataset2020[2,]), na.rm = TRUE):max(as.numeric(lcreedataset2020[2,]), na.rm = TRUE)){
   print(year)
-  i<- year -  min(as.numeric(lcreedataset2019[2,]), na.rm = TRUE) + 1
+  i<- year -  min(as.numeric(lcreedataset2020[2,]), na.rm = TRUE) + 1
   print(i)
-  data <- lcreedataset2019[c(1,2,((i*4)-1):(((i*4)-1)+3))]
+  data <- lcreedataset2020[c(1,2,((i*4)-1):(((i*4)-1)+3))]
   data$Year <- year
   lcrelist[[i]] <- data
 }
@@ -42,24 +42,24 @@ write.csv(newdata, "Output/LCRE/LCRE.csv", row.names = FALSE)
 
 
 
-lcreedataset2019 <- read_excel("Data Sources/LCRE Survey/lcreedataset2019.xlsx", 
+lcreedataset2020 <- read_excel("Data Sources/LCRE Survey/lcreedataset2020.xlsx", 
                                sheet = "LCRE group & country")
 
-lcreedataset2019 <- fill(lcreedataset2019,1, .direction ="down")
-lcreedataset2019 <- fill(lcreedataset2019,2, .direction ="down")
+lcreedataset2020 <- fill(lcreedataset2020,1, .direction ="down")
+lcreedataset2020 <- fill(lcreedataset2020,2, .direction ="down")
 
-lcreedataset2019 <- as_tibble(lcreedataset2019)
+lcreedataset2020 <- as_tibble(lcreedataset2020)
 
 lcrelist <- list()
 
 
-min(as.numeric(lcreedataset2019[4,]), na.rm = TRUE)
+min(as.numeric(lcreedataset2020[4,]), na.rm = TRUE)
 
-for (year in min(as.numeric(lcreedataset2019[4,]), na.rm = TRUE):max(as.numeric(lcreedataset2019[4,]), na.rm = TRUE)){
+for (year in min(as.numeric(lcreedataset2020[4,]), na.rm = TRUE):max(as.numeric(lcreedataset2020[4,]), na.rm = TRUE)){
   print(year)
-  i<- year -  min(as.numeric(lcreedataset2019[4,]), na.rm = TRUE) + 1
+  i<- year -  min(as.numeric(lcreedataset2020[4,]), na.rm = TRUE) + 1
   print(i)
-  data <- lcreedataset2019[c(1,2,3,((i*4)):(((i*4))+3))]
+  data <- lcreedataset2020[c(1,2,3,((i*4)):(((i*4))+3))]
   data$Year <- year
   lcrelist[[i]] <- data
 }
